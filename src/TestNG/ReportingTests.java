16ae319649d3467f0ebf2f3deac3bfae1b0207f3
igouda <i.gouda.ig@gmail.com>
2017-07-25 22:39:51 +0200

    First Commit

---
+++ src/TestNG/ReportingTests.java
@@ -0,0 +1,44 @@
+package TestNG;
+
+
+import org.testng.Assert;
+import org.testng.SkipException;
+import org.testng.annotations.Test;
+
+public class ReportingTests {
+	
+@Test
+public void TestCase1(){
+	System.out.println("Test Case 1");
+}
+
+@Test
+public void TestCase2(){
+	
+	Assert.assertEquals(1, 2);
+	System.out.println("Test Case 2");
+}
+
+@Test
+public void TestCase3(){
+	System.out.println("Test Case 3");
+	throw new SkipException("Skipped TC 3");
+}
+
+@Test
+public void TestCase4(){
+	try
+	{
+		Assert.assertTrue(4<1, "False Condition in TC 4");
+		System.out.println("Test Case 4");
+	}
+	catch(Throwable t)
+	{
+		System.out.println(t);
+	}
+	System.out.println(" False Condition Test Case 4");
+	
+}
+	
+
+}
